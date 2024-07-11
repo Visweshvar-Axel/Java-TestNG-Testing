8eadbc07f314a3839be6735bb8bf9e767d5620d0
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-12 01:43:00 +0530

    @test

--- src/TestNGAnnotations/SampleTestClass.java
+++ src/TestNGAnnotations/SampleTestClass.java
@@ -1,6 +1,10 @@
 package TestNGAnnotations;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
 public class SampleTestClass {
+	@Test
 	public void sampleTest() {
 		System.out.println("this is TestNG annotation");
 	}
