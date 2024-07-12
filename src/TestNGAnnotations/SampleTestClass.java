aa6326a5985ca6d1952f56d57d9afe8a06a742b7
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-12 14:34:33 +0530

    Annotation

--- src/TestNGAnnotations/SampleTestClass.java
+++ src/TestNGAnnotations/SampleTestClass.java
@@ -1,11 +1,33 @@
 package TestNGAnnotations;
 
-import org.testng.annotations.AfterClass;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SampleTestClass {
+	
+	//ctrl +shift + o -- to resolve dependencies
 	@Test
-	public void sampleTest() {
-		System.out.println("this is TestNG annotation");
+	public void sampleTest1() {
+		System.out.println("this is TestNG annotation1");
+	}
+	@BeforeTest
+	public void sampleTest2() {
+		System.out.println("this is TestNG annotation2");
+	}
+	@AfterTest
+	public void sampleTest3() {
+		System.out.println("this is TestNG annotation3");
+	}
+	@Test
+	public void sampleTest4() throws Exception {
+		System.out.println("this is TestNG annotation4");
+		throw new Exception("Failure");
+	}
+	@Test
+	public void sampleTest5() throws Exception {
+		System.out.println("this is TestNG annotation4");
+		throw new SkipException("Failure");
 	}
 }
