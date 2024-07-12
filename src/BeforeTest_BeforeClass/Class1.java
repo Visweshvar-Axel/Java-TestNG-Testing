cd00799079239af246aca4af917a81f7e11a85e1
Visweshvar-Axel <118894317+Visweshvar-Axel@users.noreply.github.com>
2024-07-12 17:50:35 +0530

    run through java using xml

--- src/BeforeTest_BeforeClass/Class1.java
+++ src/BeforeTest_BeforeClass/Class1.java
@@ -1,8 +1,20 @@
 package BeforeTest_BeforeClass;
 
+import java.util.List;
+
+import org.testng.TestNG;
 import org.testng.annotations.Test;
 
+import com.beust.jcommander.internal.Lists;
+
 public class Class1 extends BeforeTestAndBeforeClassTest {
+	  public static void main(String[] args) {
+		  TestNG testing = new TestNG();
+		  List<String> suites = Lists.newArrayList();
+		  suites.add("C:\\Users\\VISWA\\eclipse-workspace\\TestNGProject\\testng.xml");
+		  testing.setTestSuites(suites);
+		  testing.run();
+	  }
 	  @Test
 	  public void f1() {
 		  System.out.println("In Test3");
